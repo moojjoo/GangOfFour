f3f1a1d7d4c4454df78222e8bc0fc9ad39303a8a
Robert B. Dannelly, Jr <robertdannelly@gmail.com>
2017-10-09 16:45:23 -0400

    First Commit of Code

---
+++ GangOfFour/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DoFactory.GangOfFour.Singleton.Structural
+{
+    /// <summary>
+    /// MainApp startup class for Structural Test
+    /// Singleton Design Pattern.
+    /// </summary>
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application RBD Change.
+        /// THis is a change TEST TEST TEST Test another test
+        /// </summary>
+        static void Main()
+        {
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        // Constructor is 'protected'
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
